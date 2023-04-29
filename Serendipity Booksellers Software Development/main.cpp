a39cb8ca90419416d7b44b9b235a30cdc25852db
Spacetastics <noahcil@gmail.com>
2023-04-29 14:08:08 -0400

    Initial Commit

---
+++ Serendipity Booksellers Software Development/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Serendipity Booksellers Software Development
+//
+//  Created by Noah Cil on 4/29/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
