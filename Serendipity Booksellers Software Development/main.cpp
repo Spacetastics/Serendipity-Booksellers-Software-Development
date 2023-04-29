6afccfc8f910a320b22ffcaf4a2d146b6d7f513f
Spacetastics <noahcil@gmail.com>
2023-04-29 14:10:33 -0400

    pasted stuff from previous file to new project file

--- Serendipity Booksellers Software Development/main.cpp
+++ Serendipity Booksellers Software Development/main.cpp
@@ -2,13 +2,24 @@
 //  main.cpp
 //  Serendipity Booksellers Software Development
 //
-//  Created by Noah Cil on 4/29/23.
+//  Created by Noah Cil on 3/6/23.
 //
 
 #include <iostream>
+//cmath, iomanip, use setw function or the left justified equivalent to line up columns in console
+//cout << left << setw(spaces) << output
+//fixed, setprecision()
+//showpoint
+//getline(cin, stringvariable)
+//cin.get or cin.get(char) for characters
+//cin.ignore to skip over stuff stored in keyboard buffer before using cin.get
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    
+    for (int num = 0; num < 5; num++) {
+        cout << "bruh";
+    }
+    
     return 0;
 }
