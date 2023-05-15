3ee9cbfb66d0df231be2582a9ed5e4b62dd19259
Spacetastics <noahcil@gmail.com>
2023-05-15 18:57:38 -0400

    cleaned up and finalized bookinfo.cpp for project 1

--- Serendipity Booksellers Software Development/bookinfo.cpp
+++ Serendipity Booksellers Software Development/bookinfo.cpp
@@ -1,20 +1,26 @@
-//
-//  bookinfo.cpp
-//  Serendipity Booksellers Software Development
-//
-//  Created by Noah Cil on 4/29/23.
-//
+/*
+    File: bookinfo.cpp
+    Project: Serendipity Booksellers Software Development
+    Purpose: Displays a screen that shows information for a book
+    Author: Noah Cil
+    Professor: Ping-Wei Tsai
+    Class: CMSC 226 (30145)
+ */
 
 #include <iostream>
 #include <iomanip>
 
 using namespace std;
 
-//int main() {
-//    
-//    cout << right << setw(26) << "Serendipity Booksellers" << endl;
-//    cout << right << setw(22.5) << "Book Information" << endl << endl;
-//    
-//    cout << "ISBN: " << endl << "Title: " << endl << "Author: " << endl << "Publisher: " << endl << "Date Added: " << endl << "Quantity-On-Hand: " << endl << "Wholesale Cost: " << endl << "Retail Price: " << endl;
-//    
-//}
+//main method must be commented out at the moment because I am using separate files for everything
+
+int main() {
+    
+    //print menu header
+    cout << right << setw(26) << "Serendipity Booksellers" << endl;
+    cout << right << setw(22.5) << "Book Information" << endl << endl;
+    
+    //prints each type of book data on its own line, no data input yet
+    cout << "ISBN: " << endl << "Title: " << endl << "Author: " << endl << "Publisher: " << endl << "Date Added: " << endl << "Quantity-On-Hand: " << endl << "Wholesale Cost: " << endl << "Retail Price: " << endl;
+    
+}
