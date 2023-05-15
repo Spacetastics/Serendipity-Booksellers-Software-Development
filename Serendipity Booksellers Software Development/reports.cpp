0814962a23d280f9cfdd79795d084ca8ebe8c6bb
Spacetastics <noahcil@gmail.com>
2023-05-15 19:04:40 -0400

    cleaned up and finalized reports.cpp for project 1

--- Serendipity Booksellers Software Development/reports.cpp
+++ Serendipity Booksellers Software Development/reports.cpp
@@ -1,24 +1,31 @@
-//
-//  reports.cpp
-//  Serendipity Booksellers Software Development
-//
-//  Created by Noah Cil on 4/29/23.
-//
+/*
+    File: reports.cpp
+    Project: Serendipity Booksellers Software Development
+    Purpose: Displays the reports menu screen with 7 options, and allows the user to input a choice.
+    Author: Noah Cil
+    Professor: Ping-Wei Tsai
+    Class: CMSC 226 (30145)
+ */
 
 #include <iostream>
 #include <iomanip>
 
 using namespace std;
 
+//main method must be commented out at the moment because I am using separate files for everything
+
 //int main() {
-//    
+//
+//    //print header
 //    cout << right << setw(26) << "Serendipity Booksellers" << endl;
 //    cout << right << setw(18) << "Reports" << endl << endl;
-//    
+//
+//    //print seven options and ask user to input choice
 //    cout << "1. Inventory Listing" << endl << "2. Inventory Wholesale Value" << endl << "3. Inventory Retail Value" << endl << "4. Listing by Quantity" << endl << "5. Listing by Cost" << endl << "6. Listing by Age" << endl << "7. Return to Main Menu" << endl << endl << "Enter Your Choice: ";
-//    
+//
+//    //makes an int variable to store the user's choice in, and gets it with cin
 //    int choice;
-//    //not checking for proper input
+//    //no input validaton at the moment
 //    cin >> choice;
-//    
+//
 //}
