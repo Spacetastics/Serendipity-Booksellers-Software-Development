6b4839500d3141029bf0cf37f814c282e3f2fe3e
Spacetastics <noahcil@gmail.com>
2023-05-15 18:38:37 -0400

    cleaned up and finalized cashier.cpp for project 1

--- Serendipity Booksellers Software Development/cashier.cpp
+++ Serendipity Booksellers Software Development/cashier.cpp
@@ -1,78 +1,86 @@
-//
-//  cashier.cpp
-//  Serendipity Booksellers Software Development
-//
-//  Created by Noah Cil on 4/29/23.
-//
+/*
+    File: cashier.cpp
+    Project: Serendipity Booksellers Software Development
+    Purpose: Asks the user for the information for one book, and then displays the sale slip with that information
+    Author: Noah Cil
+    Professor: Ping-Wei Tsai
+    Class: CMSC 226 (30145)
+ */
 
 #include <iostream>
 #include <iomanip>
 
 using namespace std;
 
+//main method must be commented out at the moment because I am using separate files for everything
+
 //int main() {
-//    //getting sales data
+//    //no input validation at the moment
+//
+//    //print out header
 //    cout << right << setw(26) << "Serendipity Booksellers" << endl;
 //    cout << right << setw(21.5) << "Cashier Module" << endl << endl;
+//
+//    //get sales data:
+//
+//    //get sale date
 //    cout << "Date: ";
 //    string date;
-//    //assumes user will enter in MM//DD//YY format
-////    cin >> date;
 //    getline(cin, date);
+//
+//    //get number of this book purchased
 //    cout << "Quantity of Book: ";
 //    int quantity;
-//    //assumes user will input a number
 //    cin >> quantity;
-//    cin.ignore(numeric_limits<streamsize>::max(), '\n'); //ignore remaining input
+//    cin.ignore(); //ignore remaining '\n' char stored in buffer
+//
+//    //get book ISBN
 //    cout << "ISBN: ";
 //    string ISBN;
-////    cin >> ISBN;
 //    getline(cin, ISBN);
+//
+//    //get book title
 //    cout << "Title: ";
 //    string title;
-////    cin >> title;
 //    getline(cin, title);
+//
+//    //get book price
 //    cout << "Price: ";
 //    double price;
 //    cin >> price;
 //    cout << endl;
 //
-//    //calculations
+//    //price calculations
 //    double subtotal = price * quantity;
 //    double tax = subtotal * 0.06;
 //    double total = subtotal + tax;
 //
 //    //sales slip
-////    cout << "Serendipity Book Sellers" << endl << endl;
-////    cout << "Date: " + date << endl << endl;
-////    cout << "Qty ISBN     Title          Price     Total" << endl;
-////    cout << "-------------------------------------------" << endl << endl << endl;
-////    cout << quantity << " " << ISBN << " " << title << " " << price << " " << subtotal;
-////    cout << right << setw(18) << "Subtotal";
-////    cout << left << setw(10) << subtotal;
-////    cout << endl << right << setw(13) << "Tax";
-////    cout << left << setw(10) << tax;
-////    cout << endl << right << setw(15) << "Total";
-////    cout << left << setw(10) << total << endl << endl;
-////
-////
-////    cout << "Thank You for Shopping at Serendipity!" << endl;
-//
 //    cout << "Serendipity Book Sellers" << endl << endl;
 //    cout << "Date: " << date << endl << endl;
 //    cout << "Qty  ISBN           Title                               Price    Total    " << endl;
 //    cout << "--------------------------------------------------------------------------" << endl;
 //    cout << fixed << setprecision(2);
-//    cout << left << setw(5) << quantity << setw(15) << ISBN << setw(36) << title << "$ " << setw(7) << price << "$ " << setw(7) << subtotal << endl << endl;
+//    cout << left << setw(5) << quantity << setw(15) << ISBN << setw(36) << title << "$ " << setw(7) << price << "$ " << setw(7) << subtotal << endl << endl << endl;
 //    cout << right << setw(21) << "Subtotal" << setw(46) << "$ " << subtotal << endl;
 //    cout << right << setw(16) << "Tax" << setw(52) << "$  " << tax << endl;
 //    cout << right << setw(18) << "Total" << setw(49) << "$ " << total << endl << endl;
 //
 //    cout << "Thank You for Shopping at Serendipity!" << endl;
 //
+//
+//    //NOTES:
+//
+//    //the presentation of the sales slip can be automated depending on the
+//    //sizes of the biggest book entries for the values that are not constant.
+//    //values like the title, for example, vary and the title section can be set to the size of the largest title + couple spaces.
+//    //values like the ISBN will always be the same size so that can be constant
+//
 //    //since ISZBN is a standard size, and QTY is predictable and so is price, use stw to format within the line to give adequate spacing
 //    //all ISBN numbers are 13 chars long
 //    //34 is my longest title length
 //    //three bottom lines start 14 spaces in
 //    //66 spaces from start to last dollar sign
+//
+//    //date formatting
 //}
