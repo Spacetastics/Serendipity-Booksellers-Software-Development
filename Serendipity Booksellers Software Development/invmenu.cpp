70e7c8c25f8df9b36455d431bc6111b944dd791e
Spacetastics <noahcil@gmail.com>
2023-05-15 18:52:28 -0400

    cleaned up and finalized invmenu.cpp for project 1

--- Serendipity Booksellers Software Development/invmenu.cpp
+++ Serendipity Booksellers Software Development/invmenu.cpp
@@ -1,23 +1,30 @@
-//
-//  invmenu.cpp
-//  Serendipity Booksellers Software Development
-//
-//  Created by Noah Cil on 4/29/23.
-//
+/*
+    File: invmenu.cpp
+    Project: Serendipity Booksellers Software Development
+    Purpose: Displays the inventory menu screen with four options, and allows the user to input a choice.
+    Author: Noah Cil
+    Professor: Ping-Wei Tsai
+    Class: CMSC 226 (30145)
+ */
 
 #include <iostream>
 #include <iomanip>
 
 using namespace std;
 
+//main method must be commented out at the moment because I am using separate files for everything
+
 //int main() {
 //    
+//    //print header
 //    cout << right << setw(26) << "Serendipity Booksellers" << endl;
 //    cout << right << setw(23.5) << "Inventory Database" << endl << endl;
 //    
+//    //print four options and ask user to enter choice
 //    cout << "1. Look Up a Book" << endl << "2. Add a Book" << endl << "3. Edit a Book's Record" << endl << "4. Delete a Book" << endl << "5. Return to the Main Menu" << endl << endl << "Enter Your Choice: ";
 //
+//    //makes an int variable to store the user's choice in, and gets it with cin
 //    int choice;
-//    //not checking for proper input
+//    //no input validation at the moment
 //    cin >> choice;
 //}
