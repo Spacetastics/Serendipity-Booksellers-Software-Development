481d408136729187e7f9561d7d63e9acdf34e33a
Spacetastics <noahcil@gmail.com>
2023-05-15 21:39:39 -0400

    project 2 part 5 code implemented for invmenu.cpp

--- Serendipity Booksellers Software Development/invmenu.cpp
+++ Serendipity Booksellers Software Development/invmenu.cpp
@@ -16,43 +16,52 @@ using namespace std;
 
 //int main() {
 //
-//    //print header
-//    cout << right << setw(26) << "Serendipity Booksellers" << endl;
-//    cout << right << setw(23.5) << "Inventory Database" << endl << endl;
-//
-//    //print four options and ask user to enter choice
-//    cout << "1. Look Up a Book" << endl << "2. Add a Book" << endl << "3. Edit a Book's Record" << endl << "4. Delete a Book" << endl << "5. Return to the Main Menu" << endl << endl << "Enter Your Choice: ";
-//
-//    //makes an int variable to store the user's choice in, and gets it with cin
+//    //makes an int variable to store the user's choice
 //    int choice;
-//    cin >> choice;
+//    //continually asks the user to enter a choice until they enter 5 (return to main menu)
+//    do {
+//        
+//        //print header
+//        cout << right << setw(26) << "Serendipity Booksellers" << endl;
+//        cout << right << setw(23.5) << "Inventory Database" << endl << endl;
 //
-//    //basic input validation, does not check whether what was entered is an integer
-//    if (choice < 1 || choice > 5)
-//        cout << endl << "Please enter a number in the range 1-5." << endl;
-//    else {
+//        //print five options
+//        cout << "1. Look Up a Book" << endl << "2. Add a Book" << endl << "3. Edit a Book's Record" << endl << "4. Delete a Book" << endl << "5. Return to the Main Menu" << endl << endl;
+//        
+//        //asks user for input and stores console input in choice
+//        cout << "Enter Your Choice: ";
+//        cin >> choice;
+//        
+//        //basic input validation, does not check whether what was entered is an integer
+//        while (choice < 1 || choice > 5) {
+//            cout << endl << "Please enter a number in the range 1-5: ";
+//            cin >> choice;
+//            cin.ignore(); //this stops the console output from being flooded by the above output due to my second endl insertion in the following switch statement
+//        }
+//        
 //        cout << endl << "You selected item ";
 //        switch (choice) {
 //            case 1:
-//                cout << "1." << endl;
+//                cout << "1." << endl << endl;
 //                break;
 //            case 2:
-//                cout << "2." << endl;
+//                cout << "2." << endl << endl;
 //                break;
 //            case 3:
-//                cout << "3." << endl;
+//                cout << "3." << endl << endl;
 //                break;
 //            case 4:
-//                cout << "4." << endl;
+//                cout << "4." << endl << endl;
 //                break;
 //            case 5:
-//                cout << "5." << endl;
+//                cout << "5." << endl << endl;
 //                break;
 //            //this should never be reached
 //            default:
 //                cout << "ERROR" << endl;
 //                break;
 //        }
-//    }
-//
+//        
+//    } while (choice != 5);
+//    
 //}
