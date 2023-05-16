295f81d040061a0aefde6f4d2077cf4957cee235
Spacetastics <noahcil@gmail.com>
2023-05-15 22:21:07 -0400

    project 2 part 5 code implemented for cashier.cpp

--- Serendipity Booksellers Software Development/cashier.cpp
+++ Serendipity Booksellers Software Development/cashier.cpp
@@ -16,58 +16,74 @@ using namespace std;
 
 //int main() {
 //    //no input validation at the moment
+//    
+//    //makes an int variable to store a users choice
+//    int choice;
+//    do {
+//        
+//        //print out header
+//        cout << right << setw(26) << "Serendipity Booksellers" << endl;
+//        cout << right << setw(21.5) << "Cashier Module" << endl << endl;
 //
-//    //print out header
-//    cout << right << setw(26) << "Serendipity Booksellers" << endl;
-//    cout << right << setw(21.5) << "Cashier Module" << endl << endl;
+//        //get sales data:
 //
-//    //get sales data:
+//        //get sale date
+//        cout << "Date: ";
+//        string date;
+//        getline(cin, date);
 //
-//    //get sale date
-//    cout << "Date: ";
-//    string date;
-//    getline(cin, date);
+//        //get number of this book purchased
+//        cout << "Quantity of Book: ";
+//        int quantity;
+//        cin >> quantity;
+//        cin.ignore(); //ignore remaining '\n' char stored in buffer
 //
-//    //get number of this book purchased
-//    cout << "Quantity of Book: ";
-//    int quantity;
-//    cin >> quantity;
-//    cin.ignore(); //ignore remaining '\n' char stored in buffer
+//        //get book ISBN
+//        cout << "ISBN: ";
+//        string ISBN;
+//        getline(cin, ISBN);
 //
-//    //get book ISBN
-//    cout << "ISBN: ";
-//    string ISBN;
-//    getline(cin, ISBN);
+//        //get book title
+//        cout << "Title: ";
+//        string title;
+//        getline(cin, title);
 //
-//    //get book title
-//    cout << "Title: ";
-//    string title;
-//    getline(cin, title);
+//        //get book price
+//        cout << "Price: ";
+//        double price;
+//        cin >> price;
+//        cout << endl;
 //
-//    //get book price
-//    cout << "Price: ";
-//    double price;
-//    cin >> price;
-//    cout << endl;
+//        //price calculations
+//        double subtotal = price * quantity;
+//        double tax = subtotal * 0.06;
+//        double total = subtotal + tax;
 //
-//    //price calculations
-//    double subtotal = price * quantity;
-//    double tax = subtotal * 0.06;
-//    double total = subtotal + tax;
+//        //sales slip
+//        cout << "Serendipity Book Sellers" << endl << endl;
+//        cout << "Date: " << date << endl << endl;
+//        cout << "Qty  ISBN           Title                               Price    Total    " << endl;
+//        cout << "--------------------------------------------------------------------------" << endl;
+//        cout << fixed << setprecision(2);
+//        cout << left << setw(5) << quantity << setw(15) << ISBN << setw(36) << title << "$ " << setw(7) << price << "$ " << setw(7) << subtotal << endl << endl << endl;
+//        cout << right << setw(21) << "Subtotal" << setw(46) << "$ " << subtotal << endl;
+//        cout << right << setw(16) << "Tax" << setw(52) << "$  " << tax << endl;
+//        cout << right << setw(18) << "Total" << setw(49) << "$ " << total << endl << endl;
 //
-//    //sales slip
-//    cout << "Serendipity Book Sellers" << endl << endl;
-//    cout << "Date: " << date << endl << endl;
-//    cout << "Qty  ISBN           Title                               Price    Total    " << endl;
-//    cout << "--------------------------------------------------------------------------" << endl;
-//    cout << fixed << setprecision(2);
-//    cout << left << setw(5) << quantity << setw(15) << ISBN << setw(36) << title << "$ " << setw(7) << price << "$ " << setw(7) << subtotal << endl << endl << endl;
-//    cout << right << setw(21) << "Subtotal" << setw(46) << "$ " << subtotal << endl;
-//    cout << right << setw(16) << "Tax" << setw(52) << "$  " << tax << endl;
-//    cout << right << setw(18) << "Total" << setw(49) << "$ " << total << endl << endl;
+//        //asks user for input and stores in the bool repeat
+//        cout << "Would you like to process another transaction? Yes [1] or No [0]: ";
+//        cin >> choice;
+//        while (choice != 0 && choice != 1) {
+//            cout << endl << "Please enter 1 or 0: ";
+//            cin >> choice;
+//        }
+//        
+//        cout << endl; cin.ignore(); //formatting, so next print of menu is spaced away from previous sale slip
+//        
+//    } while (choice == 1);
 //
 //    cout << "Thank You for Shopping at Serendipity!" << endl;
-//
+//    
 //    return 0;
 //
 //    //NOTES:
